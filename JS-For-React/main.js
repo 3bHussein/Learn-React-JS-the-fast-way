994c58fa191ae023c5973445c154f45113592ef2
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-28 00:47:38 +0400

    Update main.js
    Try Arrow Function and Rest Operator

--- JS-For-React/main.js
+++ JS-For-React/main.js
@@ -10,10 +10,78 @@ console.log(window.a); // ✅ 1
 console.log(window.b); // ❌ undefined
 */ 
 
-if(true){var y =30;}
-console.log(y);
+if(true){let x =30;}
+// console.log(y);
+console.log(x);
 
 
+
+const book={title : 'hello world'};
+
+book.title='hello world 2';
+console.log(book.title)
+console.log(book)
 };
+
+
+
+
+
+
+function addTraditional(a,b ) {
+    
+    
+
+    return console.log(a+b);
+}
+
+const test = () => console.log('test');
+// console.log(addTraditional(5,4));
+console.log('---------------');
+addTraditional(3,5);
+console.log('---------------');
+
+
+test();
 main();
 
+
+
+
+
+
+console.log('---------------');
+
+
+
+const ids=[1,2,3,4];
+
+
+ids.forEach((id)=>{id++;
+    console.log(id);
+});
+
+console.log(ids[0]);
+console.log('----<<<Arrow Function>>>------');
+// Arrow funcation
+
+// test1(); while make a error becuase it's const can't access 
+
+const test1 =() => {console.log('test Arrow Function')}
+
+test1();
+
+
+console.log('-------<<<Rest Operator------');
+
+// Rest Operator
+function RestOPTest(x,z,...y){
+    console.log(x);
+    console.log(z);
+    console.log(y);
+
+
+}
+
+RestOPTest('ahmed','ali','mohamed','noah','zina')
+console.log('-------<<<>>>--------');
