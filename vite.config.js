cc1d076e1a7fd4fca199b53d5600e0c6526dd423
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-29 09:55:27 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/Learn-React-JS-the-fast-way/',
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
