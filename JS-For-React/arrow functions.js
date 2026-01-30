d6a92a023db6da2e607f17ff749b4bad8eb49bd1
hussein ahmed <39864308+3bHussein@users.noreply.github.com>
2026-01-30 19:09:44 +0400

    feat: add dynamic rating styles to MovieCard

--- JS-For-React/arrow functions.js
+++ JS-For-React/arrow functions.js
@@ -36,3 +36,18 @@ let sum =(r)=> console.log(r);
 
 sum(87);
 HandlError(3);
+
+
+
+let sum2 =(x,y)=>x+y
+console.log('0000000000000000000000')
+
+console.log(sum2(5,3))
+
+
+
+let sum2Random=()=> Math.random(0)
+function Rando() {
+    Math.random();
+}
+ 
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,9 +4,16 @@ import '../styles.css';
 
 function MovieCard({movie}){
 
-    const ImageHaddle= (e)=>{
-        e.target.src= "images/default.jpg";
-    }
+    const ImageHandler= (e)=>e.target.src= "images/default.jpg";
+    const GetRate=(rating)=>{
+        if(rating>=8)return 'rating-good';
+        if (rating >=5 && rating<8)return 'rating-ok';
+        return 'rating-bad';
+        // that one not work i don't know i will search for it later "XD"
+        // i know why because only else without return will not work {"Syntax Error"}
+        // else{} 'rating-bad';
+
+    };
 
 
     return(
@@ -14,12 +21,12 @@ function MovieCard({movie}){
           <div key={movie.id} className="movie-card">
                     <img src={`images/${movie.image}`}
                      alt={movie.title}
-                     onError={ImageHaddle}
+                     onError={ImageHandler}
                      />
 
                     <h3 className="movie-card-title">{movie.title}</h3>
                     <div className="movie-card-genre">{movie.genre}</div>
-                    <div className="movie-card-rating">{movie.rating}</div>
+                    <div className={`movie-card-rating ${GetRate(movie.rating)}`}>{movie.rating}</div>
                 </div>
     );
 }
